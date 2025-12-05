e48434a7a42cd53032b56d82986410d68ad2a540
God Given (GG) <parhomcukbogdan81@gmail.com>
2025-12-05 10:05:34 +0200

    Add files via upload

---
+++ CW_lesson_4.py
@@ -0,0 +1,31 @@
+import cv2
+img = cv2.imread('images/messi.jpeg')
+
+scale = 4
+img = cv2.resize(img, (img.shape[1] // scale, img.shape[0] // scale))
+
+img_copy = img.copy()
+
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)# градація в сірий
+img = cv2.GaussianBlur(img, (5, 5), 1)# розмиття
+img = cv2.equalizeHist(img)
+img_edges = cv2.Canny(img, 150, 150)# виявлення краї
+
+contours, hierarchy = cv2.findContours(img_edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+for cnt in contours:
+    area = cv2.contourArea(cnt)
+    if area > 200:
+        x, y, w, h = cv2.boundingRect(cnt)
+
+        cv2.drawContours(img_copy, [cnt], -1, (0, 255, 0), 2)
+
+        cv2.rectangle(img_copy, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        text_y = y -10 if y - 10 > 10 else y + 10
+        text = f'x:{x} y:{y} s:{int(area)}'
+        cv2.putText(img_copy, text, (x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+
+cv2.imshow('img', img_copy)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
